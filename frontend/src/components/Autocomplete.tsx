e29ca23e4e58204fea1f73ca1f93ebc90f323dbf
Ritam Nandi <nandi.ritamnandi@gmail.com>
2026-01-15 16:27:34 -0800

    Prep for GitHub pages deployment

--- frontend/src/components/Autocomplete.tsx
+++ frontend/src/components/Autocomplete.tsx
@@ -9,7 +9,6 @@ interface AutocompleteProps {
 
 export const Autocomplete: React.FC<AutocompleteProps> = ({ onGuess, disabled }) => {
     const [query, setQuery] = useState("");
-    const [allPlayers, setAllPlayers] = useState<string[]>([]);
     const [results, setResults] = useState<string[]>([]);
     const [showSuggestions, setShowSuggestions] = useState(false);
     
@@ -17,7 +16,6 @@ export const Autocomplete: React.FC<AutocompleteProps> = ({ onGuess, disabled })
 
     useEffect(() => {
         getPlayers().then(data => {
-            setAllPlayers(data);
             fuseRef.current = new Fuse(data, {
                 threshold: 0.4, // match sens
             });
